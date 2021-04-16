923db5b232383e9d2305ac12a5b15b711c8e1b10
Krupa Dhruva <krupadhruva@gmail.com>
2021-04-15 17:34:45 -0700

    Finished Lab A and LM_Chapter2.1 code

--- LM_chapters/Lab_2/circlearea.cpp
+++ LM_chapters/Lab_2/circlearea.cpp
@@ -1,7 +1,7 @@
 // This program will output the circumference and area
 // of the circle with a given radius.
 
-// PLACE YOUR NAME HERE
+// Krupa Dhruva
 
 #include <iostream>
 using namespace std;
@@ -11,16 +11,18 @@ const double RADIUS = 5.4;
 
 int main()
 {
-	area								// definition of area of circle 
+	float area;								// definition of area of circle
 	float circumference;				// definition of circumference 
 	circumference = 2 * PI * RADIUS;	// computes circumference
-	area = ;							// computes area
+	area = PI * (RADIUS * RADIUS);							// computes area
 
 	// Fill in the code for the cout statement that will output (with description)
 	// the circumference
+	cout << "The circumference of the circle is " << circumference << endl;
 
 	// Fill in the code for the cout statement that will output (with description)
 	// the area of the circle
+	cout << "The area of the circle is " << area << endl;
 
 	return 0;
 }
---
+++ Labs/Lab_A.cpp
@@ -0,0 +1,22 @@
+/* A program that stores the integers 50 and 100 in variables, and stores
+ * the sum of these two in a variable named total. Then it uses cout to
+ * output the total.
+*/
+
+// Krupa Dhruva
+// Program Output:
+// "Total is 150"
+
+#include <iostream> // for C++ input & output
+using namespace std;
+
+int main() {
+    int firstNum = 50;
+    int secondNum = 100;
+
+    int total = firstNum + secondNum;
+
+    cout << "Total is " << total << endl;
+}
+
+
