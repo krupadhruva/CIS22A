7f100fcd489a3bec6c88c9f238f95b8c36c060ae
Krupa Dhruva <krupadhruva@gmail.com>
2021-04-22 22:10:18 -0700

    LM_Chapter 3.4 Lab: Uses type casting to maintain precision of values

--- LM_chapters/Lab_3/batavg.cpp
+++ LM_chapters/Lab_3/batavg.cpp
@@ -1,7 +1,7 @@
 // This program will determine the batting average of a player.
 // The number of hits and at bats are set internally in the program.
 
-// PLACE YOUR NAME HERE
+// Krupa Dhruva
 
 #include <iostream>
 using namespace std;
@@ -9,12 +9,13 @@ using namespace std;
 const int AT_BAT = 421;
 const int HITS = 123;
 
-int main()
-{
-	int batAvg;
+int main() {
+    // needs to be double or float to hold values with decimals
+    double batAvg;
 
-	batAvg = HITS / AT_BAT									// an assignment statement 
-	cout << "The batting average is " << batAvg << endl;	// output the result
+    // type casting to double to match type of variable batAvg
+    batAvg = static_cast<double>(HITS) / AT_BAT; // an assignment statement
+    cout << "The batting average is " << batAvg << endl; // output the result
 
-	return 0;
+    return 0;
 }
