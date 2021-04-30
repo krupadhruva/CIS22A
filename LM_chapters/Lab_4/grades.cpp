2d18017e7bcab1bd0aa2c2a64a48366d167a2e3f
Krupa Dhruva <krupadhruva@gmail.com>
2021-04-29 20:58:24 -0700

    LM_Chapter 4.1 Lab: Uses if-else statements to decide whether inputted avg score is a 'pass' or 'fail'

--- LM_chapters/Lab_4/grades.cpp
+++ LM_chapters/Lab_4/grades.cpp
@@ -1,23 +1,23 @@
 // This program prints "You Pass" if a student's average is
 // 60 or higher and prints "You Fail" otherwise
 
-// PLACE YOUR NAME HERE
+// Krupa Dhruva
 
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	float average;	// holds the grade average
+int main() {
+    float average; // holds the grade average
 
-	cout << "Input your average:" << endl;
-	cin >> average;
+    cout << "Input your average:" << endl;
+    cin >> average;
 
-	if (average > 60)
-		cout << "You Pass" << endl;
+    if (average >= 60) {
+        cout << "You Pass" << endl;
+    } else {
+        (average < 60);
+        cout << "You Fail" << endl;
+    }
 
-	if (average < 60)
-		cout << "You Fail" << endl;
-
-	return 0;
+    return 0;
 }
