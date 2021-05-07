5f121bfea33a299943a17f9b63de81495230a498
Krupa Dhruva <krupadhruva@gmail.com>
2021-05-06 21:27:58 -0700

    LM_Chapter 6.1 Lab 1: Function declaration and using/writing a function

--- LM_chapters/Lab_6.1/newproverb.cpp
+++ LM_chapters/Lab_6.1/newproverb.cpp
@@ -1,51 +1,65 @@
 // This program will allow the user to input from the keyboard
 // whether the last word to the following proverb should be party or country:
 // "Now is the time for all good men to come to the aid of their ___"
-// Inputting a 1 will use the word party. Any other number will use the word country.
+// Inputting a 1 will use the word party. Any other number will use the word
+// country.
 
-// PLACE YOUR NAME HERE
+// Krupa Dhruva
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-// Fill in the prototype of the function writeProverb.
+// Fill in the prototype of the function writeProverb; this is to declare the
+// function
+void writeProverb(int);
 
-int main()
-{
-	int wordCode;
+int main() {
+    int wordCode;
 
-	cout << "Given the phrase:" << endl;
-	cout << "Now is the time for all good men to come to the aid of their ___"
-		 << endl;
+    cout << "Given the phrase:" << endl;
+    cout << "Now is the time for all good men to come to the aid of their ___"
+         << endl;
 
-	cout << "Input a 1 if you want the sentence to be finished with party"
-		 << endl;
-	cout << "Input any other number for the word country" << endl;
+    cout << "Input a 1 if you want the sentence to be finished with party"
+         << endl;
+    cout << "Input any other number for the word country" << endl;
 
-	cout << "Please input your choice now" << endl;
-	cin >> wordCode; 
-	cout << endl;
+    cout << "Please input your choice now" << endl;
+    cin >> wordCode;
+    cout << endl;
 
-	writeProverb(wordCode);
+    writeProverb(wordCode);
 
-	return 0;
+    return 0;
 }
 
 //	******************************************************************************
 //	writeProverb
 //
 //	task:	  This function prints a proverb. The function takes a number
-//	          from the call. If that number is a 1 it prints "Now is the time
-//	          for all good men to come to the aid of their party."
-//	          Otherwise, it prints "Now is the time for all good men
-//	          to come to the aid of their country."
-//	data in:  code for ending word of proverb (integer)
-//	data out: no actual parameter altered
+//	          from the call. If that number is a 1 it prints "Now is the
+// time 	          for all good men to come to the aid of their party."
+// Otherwise, it prints
+//"Now is the time for all good men 	          to come to the aid of their
+// country." 	data
+// in:  code for ending word of proverb (integer) 	data out: no actual
+// parameter altered
 //
 //	*****************************************************************************
 
-void writeProverb(int number)
-{
-	// Fill in the body of the function to accomplish what is described above
+void writeProverb(int number) {
+    // Fill in the body of the function to accomplish what is described above
+    /* Put "1" on the left side in case I had accidentally done = instead of ==
+     * this way, I can ensure that there is no unwanted assigning happening
+     */
+    if (1 == number) {
+        cout << "Now is the time for all good men to come to the aid of their "
+                "party."
+             << endl;
+    } else {
+        cout << "Now is the time for all good men to come to the aid of their "
+                "country."
+             << endl;
+    }
 }
